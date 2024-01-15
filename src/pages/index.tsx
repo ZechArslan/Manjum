9faf7191fdc42f9b6b6a302023e5729a78701e3b
ZechArslan <zecharslan@gmail.com>
2024-01-15 15:43:48 +0500

    fix names

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,20 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 import Dashboard from "./dashboard";
 import { useState } from "react";
+// import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+// import "../assets/scss/theme.scss";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "@/slices";
+import { BrowserRouter } from "react-router-dom";
+// import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
+// const route11 = dynamic(() => import("react-router-dom").then((mod) => mod.BrowserRouter), {
+// ssr: false
+// });
+
+const store = configureStore({ reducer: rootReducer, devTools: true });
 
 export default function Home() {
   return (
